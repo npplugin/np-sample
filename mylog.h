62190feadfe27353a007a941ba76d76b205045f5
yang <jimyang365@outlook.com>
2021-04-13 16:02:31 +0800

    update my_log

--- mylog.h
+++ mylog.h
@@ -1,4 +1,10 @@
 #include <sstream>
 #include <windows.h>
 
-#define MY_LOG(msg) do {std::stringstream ss; ss << "[np_logs] " << __FUNCTION__ << ", " << __FILE__ << "(" << __LINE__ << "): " << msg; OutputDebugString(ss.str().c_str());} while(0)
+#define MY_LOG(fmt, ...) do { \
+char buf[2048] = { 0 }; \
+sprintf(buf, fmt, __VA_ARGS__); \
+std::stringstream ss; \
+ss << "[np_logs] " << __FUNCTION__ << ", " << __FILE__ << "(" << __LINE__ << "): " << buf; \
+OutputDebugString(ss.str().c_str());} \
+while(0)
