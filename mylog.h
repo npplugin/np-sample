0fb6b9524fcec4b90d519a5ad1127faa84872dfb
yang <jimyang365@outlook.com>
2021-04-14 09:08:07 +0800

    sprintf_s

--- mylog.h
+++ mylog.h
@@ -3,8 +3,9 @@
 
 #define MY_LOG(fmt, ...) do { \
 char buf[2048] = { 0 }; \
-sprintf(buf, fmt, __VA_ARGS__); \
+sprintf_s(buf, sizeof(buf), fmt, __VA_ARGS__); \
 std::stringstream ss; \
 ss << "[np_logs] " << __FUNCTION__ << ", " << __FILE__ << "(" << __LINE__ << "): " << buf; \
 OutputDebugString(ss.str().c_str());} \
 while(0)
+
